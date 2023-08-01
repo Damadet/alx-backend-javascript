7ac31b80253b374ea6c4c3cdc194c6ad432a3a97
damadet <adedokundamilare41@gmail.com>
2023-08-01 20:15:24 +0200

    get students by location

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,6 @@
-export default function getStudentsByLocation() {
-  
+export default function getStudentsByLocation(list, city) {
+  if (!Array.isArray(list)) {
+    return [];
+  }
+  return list.filter((stud) => stud.location === city);
 }
