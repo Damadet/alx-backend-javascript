0ec2656e1cdceacfe3cda4cb34d19308ed100dfa
damadet <adedokundamilare41@gmail.com>
2023-08-01 22:24:51 +0200

    upgrade grade by city

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,10 @@
+export default function updateStudentGradeByCity(list, city, newGrades) {
+  if (!Array.isArray(list) || !Array.isArray(newGrades)) {
+    return [];
+  }
+  const newList = list.filter((stud) => stud.location === city).map((stud) => {
+    const item2 = newGrades.find((stud2) => stud.id === stud2.id);
+    return { ...stud, grade: item2 ? item2.grade : 'N/A' };
+  });
+  return newList;
+}
