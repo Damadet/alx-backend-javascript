afa5d8a3ed9fd5bab6058fc3cc6cded6e1264ed7
damadet <adedokundamilare41@gmail.com>
2023-09-21 23:54:42 +0100

    mocha

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -1,32 +1,23 @@
-const sinon = require('sinon');
-var { expect } = require('chai');
+const {describe, it} = require("mocha");
+const sinon = require("sinon");
+const sendPaymentRequestToApi = require("./5-payment");
+const expect = require("chai").expect;
 
-const sendPaymentRequestToApi = require('./5-payment');
-const Utils = require('./utils');
+describe("sendPaymentRequestToApi", function() {
+    beforeEach("Set up spy to use for each test", function() {
+	sinon.spy(console, "log");
+    });
+    afterEach("restore spy after each test", function() {
+	console.log.restore();
+    });
+    it("check that console.log is called with the right arg", function() {
+	sendPaymentRequestToApi(100, 20);
 
-describe('Hooks', function () {
-  let spyConsole;
-  beforeEach(function () {
-    // runs before each test in this block
-    spyConsole = sinon.spy(console, 'log');
-  });
+	expect(console.log.withArgs("The total is: 120").calledOnce).to.be.true;
+    });
+    it("check that console.log is called with the right arg", function() {
+	sendPaymentRequestToApi(10, 10);
 
-  afterEach(function () {
-    // runs after each test in this block
-    spyConsole.restore();
-  });
-
-  it('logs correctly with 100, 20', () => {
-    sendPaymentRequestToApi(100, 20);
-
-    expect(spyConsole.calledOnceWithExactly('The total is: 120')).to.be.true;
-    expect(spyConsole.calledOnce).to.be.true;
-  });
-
-  it('logs correctly with 10, 10', () => {
-    sendPaymentRequestToApi(10, 10);
-
-    expect(spyConsole.calledOnceWithExactly('The total is: 20')).to.be.true;
-    expect(spyConsole.calledOnce).to.be.true;
-  });
+	expect(console.log.withArgs("The total is: 20").calledOnce).to.be.true;
+    });
 });
